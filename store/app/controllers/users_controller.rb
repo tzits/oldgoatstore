8538ff8ca1d81a9062883d323f066844c7d55ad0
tzits <tzitsman@gmail.com>
2016-12-25 00:34:51 -0500

    starts user controller

--- store/app/controllers/users_controller.rb
+++ store/app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  def create
+    @user = User.create(user_params)
+  end
+  def show
+    @user = User.find(params[:id])
+    @orders = @user.orders
+    @saved = User.find_by_sql("SELECT * FROM saved_items WHERE user_id = #{params[:id]}")
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+  end
+  def destroy
+    user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password_digest, :shipping_address, :admin)
+  end
+
 end
--- store/db/schema.rb
+++ store/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161224170019) do
+ActiveRecord::Schema.define(version: 20161225051729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20161224170019) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "username"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "shipping_address"
     t.boolean  "admin"
     t.datetime "created_at",       null: false
