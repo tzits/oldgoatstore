8288620e418de3788defa4e606bcb833119861dd
tzits <tzitsman@gmail.com>
2016-12-27 00:57:15 -0500

    outlines order_items controller

--- store/app/controllers/saved_items_controller.rb
+++ store/app/controllers/saved_items_controller.rb
@@ -1,2 +1,24 @@
 class SavedItemsController < ApplicationController
+
+  def create
+    @saved = SavedItem.create(item_params)
+    if @saved.valid?
+      @saved.save
+      flash[:success] = "Item Saved"
+    else
+      flash[:error] = @saved.errors.full_messages.join(". ")
+    end
+  end
+
+  def destroy
+    SavedItem.find(params[:id]).destroy
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def item_params
+    params.require(:saved_item).permit(:item_id,:user_id)
+  end
+
 end
