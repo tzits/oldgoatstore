7cbf3d9b881127fc24e7750961260d9dad2bb1d1
tzits <tzitsman@gmail.com>
2016-12-30 00:11:07 -0800

    adds login ajax command (maybe)

--- store/app/assets/javascripts/components/loginform.es6.jsx
+++ store/app/assets/javascripts/components/loginform.es6.jsx
@@ -1,15 +1,26 @@
 var Loginform = React.createClass ({
+  setLoginInfo: function() {
+    var login_data = {
+      'username': $('input[name=name]').val(),
+      'password': $('input[name=password]').val()
+    }
+  },
   login: function(){
     console.log('clicked')
+    $.ajax({
+      type: 'POST',
+      url: '/login',
+      data: this.login_data
+    })
   },
   render () {
     return (
       <div>
         <h1>Login to Your Account</h1>
-        <label for="email">Email</label>
-        <input></input><br/>
+        <label for="username">Email</label>
+        <input name='name'></input><br/>
         <label for="password">Password</label>
-        <input></input><br/>
+        <input type='password' name='password'></input><br/>
         <button onClick={this.login}>Log In</button>
       </div>
       )
