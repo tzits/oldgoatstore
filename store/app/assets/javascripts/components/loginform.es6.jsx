558b51afc6652c8a926bc66061b5a34361dcb21b
tzits <tzitsman@gmail.com>
2016-12-29 23:41:35 -0800

    login form created

--- store/app/assets/javascripts/components/loginform.es6.jsx
+++ store/app/assets/javascripts/components/loginform.es6.jsx
@@ -1,9 +1,17 @@
-class Loginform extends React.Component {
+var Loginform = React.createClass ({
+  login: function(){
+    console.log('clicked')
+  },
   render () {
     return (
       <div>
-        Login
+        <h1>Login to Your Account</h1>
+        <label for="email">Email</label>
+        <input></input><br/>
+        <label for="password">Password</label>
+        <input></input><br/>
+        <button onClick={this.login}>Log In</button>
       </div>
       )
   }
-}
+})
