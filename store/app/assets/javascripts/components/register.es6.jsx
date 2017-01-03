774b54005f3ebce0862499e52b215e00fa35c331
tzits <tzitsman@gmail.com>
2017-01-02 20:48:45 -0800

    creates register page

--- store/app/assets/javascripts/components/register.es6.jsx
+++ store/app/assets/javascripts/components/register.es6.jsx
@@ -1,8 +1,21 @@
 var Register = React.createClass ({
+  register: function(e) {
+    e.preventDefault()
+    console.log('you got me')
+  },
   render () {
     return (
       <div>
         <h2>Welcome to the Old Goat Antique Store</h2>
+        <label for="name">Name: </label>
+        <input id='name'></input><br/>
+        <label for="address">Shipping Address: </label>
+        <input id='address'></input><br/>
+        <label for="username">Email/Username: </label>
+        <input id='username'></input><br/>
+        <label for="password">Password: </label>
+        <input type='password' id='password' name='password'></input><br/>
+        <button onClick={this.register}>Register</button>
       </div>
     )
   }
